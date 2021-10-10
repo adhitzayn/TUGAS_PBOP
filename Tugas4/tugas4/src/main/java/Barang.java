9222bf86d1b8f1e30f78a7e63cd1a2615754e1a5
adhitzayn <adhitia.zain.tif20@polban@ac.id>
2021-10-10 20:25:29 +0700

    tugas4

---
+++ Tugas4/tugas4/src/main/java/Barang.java
@@ -0,0 +1,19 @@
+public class Barang {
+    String kode_barang;
+    String nama_barang;
+    private int stok;
+    
+    //()
+    public Barang(String kode, String nama, int stk){
+        kode_barang = kode;
+        nama_barang = nama;
+        stok = stk;
+    }
+    
+    public int getStok(){
+        return stok;
+    }
+    public void addStok(){
+        this.stok = this.stok + stok;
+    }
+}
