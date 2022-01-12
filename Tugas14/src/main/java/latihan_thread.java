2a5d8a3f7c41b890a5f8efd4841ff213af91ded7
adhitzayn <adhitia.zain.tif20@polban@ac.id>
2022-01-12 18:58:12 +0700

    Tugas 14

--- Tugas14/src/main/java/latihan_thread.java
+++ Tugas14/src/main/java/latihan_thread.java
@@ -1,12 +1,13 @@
 class MyThread1 extends Thread {   
+    @Override
     public void run() {
         try {
             for (int i=0; i<10; i++) {
-                System.out.println("Thread pertama: detik ke-" + (i+1));         
+                System.out.println("Ruang 1 : PlayStation ke-" + (i+1));         
                 if (i != 9) {           
                     sleep(1000);
                 } else {
-                    System.out.println("Thread pertama selesai...\n");
+                    System.out.println("Ruang 1 selesai...\n");
                 }
             }
         } catch (InterruptedException ie) {
@@ -16,14 +17,16 @@ class MyThread1 extends Thread {
 }
 
 class MyThread2 extends Thread {   
+    @Override
     public void run() {
         try {
             for (int i=0; i<5; i++) {
-                System.out.println("Thread kedua: detik ke-" + (i+1));
+                System.out.println("Ruang 2 : PlayStation ke-" + (i+1));
                 if (i != 4) {           
-                    System.out.println();           sleep(1000);
+                    System.out.println();           
+                    sleep(1000);
                 } else {
-                    System.out.println("Thread kedua selesai...\n");
+                    System.out.println("Ruang dua selesai...\n");
                 }
             }
         } catch (InterruptedException ie) {
@@ -34,7 +37,8 @@ class MyThread2 extends Thread {
 
 class latihan_thread {
   public static void main(String[] args) {
-
+      
+      System.out.println("Sistem Rental PlayStation");
     MyThread1 t1 = new MyThread1();     t1.start();
 
     MyThread2 t2 = new MyThread2();     t2.start();
